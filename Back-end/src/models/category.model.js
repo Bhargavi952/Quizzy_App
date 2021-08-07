36d9aacf15eb112aae391061288a47429aaa4cbc
unknown <saxena1046@hotmail.com>
2021-08-07 23:55:03 +0530

    schema updated

--- Back-end/src/models/category.model.js
+++ Back-end/src/models/category.model.js
@@ -4,7 +4,8 @@ const Question = require("./question.model.js")
 const categorySchema = new mongoose.Schema({
     title: {type: String, required: true},
     questions: [{type: mongoose.Schema.Types.ObjectId, ref: Question, default:""}],
-    status: {type: Boolean, default:false} 
+    status: {type: Boolean, default:false},
+    url: {type: String, required: true}
 },
 {
     timestamps: true,
